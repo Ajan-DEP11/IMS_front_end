30b6f2a5a90647ccca91322263f0c001f020be9c
PrasadPrabash <prasad.lakman@gmail.com>
2023-11-23 01:01:00 +0530

    create teache.js file and link

---
+++ src/js/teacher.js
@@ -0,0 +1,30 @@
+function initializeToolTips(){
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+}
+
+initializeToolTips();
+
+const btnAddElm = document.querySelector('#btn-add');
+const btnClearElm = document.querySelector('#btn-clear');
+const btnAddNewCustomerElm = document.querySelector('#btnAddNewCustomer');
+const txtNameElm = document.querySelector('#txt-name');
+const txtAddressElm = document.querySelector('#txt-address');
+const txtIdElm = document.querySelector('#txt-id');
+const {API_URL} = process.env;
+
+loadAllTeachers();
+
+function loadAllTeachers(){ 
+    fetch(`${API_URL}/teachers`).then(res =>{
+        if(res.ok){
+                res.json().then(teacherList => teacherList.forEach(teacher => createTeacher(tteacherask)));
+        }else{
+            alert("Failt to load all tasks")
+        }
+
+    }).catch(err => {
+        alert('Something went wrong. Try Later');
+    });
+    
+}
