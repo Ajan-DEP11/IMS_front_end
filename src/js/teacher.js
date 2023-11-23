fba9aef6834c36aab237a5fc398964a6be9bd32c
PrasadPrabash <prasad.lakman@gmail.com>
2023-11-23 10:44:35 +0530

    Add listner to add button

--- src/js/teacher.js
+++ src/js/teacher.js
@@ -18,7 +18,7 @@ loadAllTeachers();
 function loadAllTeachers(){ 
     fetch(`${API_URL}/teachers`).then(res =>{
         if(res.ok){
-                res.json().then(teacherList => teacherList.forEach(teacher => createTeacher(tteacherask)));
+                res.json().then(teacherList => teacherList.forEach(teacher => createTeacher(teacher)));
         }else{
             alert("Failt to load all tasks")
         }
@@ -28,3 +28,43 @@ function loadAllTeachers(){
     });
     
 }
+
+btnAddElm.addEventListener('click', ()=>{
+    const name = txtNameElm.value.trim();
+    const contact = txtContElm.value.trim();
+    fetch(`${API_URL}/teachers`,{
+        method: 'POST',
+        headers: {
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify({name, contact})
+       }).then(res =>{
+        if(res.status === 201){
+            res.json().then(teacher => {
+                createNewRow(teacher);
+                txtNameElm.value ='';
+                txtContElm.value='';
+                txtNameElm.focus();
+    
+            });
+        }else{
+            alert("Fail to create teacher");
+        }
+       }).catch(err => alert("Something went wrong, try again later"));
+  
+
+});
+
+function createNewRow(teacher){
+    const trElm = document.createElement('tr');
+    document.querySelector('#tbl tbody').append(trElm);
+    trElm.innerHTML=`
+                <tr>
+                    <td>${teacher.id}</td>
+                    <td>${teacher.name}</td>
+                    <td>${teacher.contact}</td>
+                    <td><button class="delete btn btn-danger">Delete</button></td>
+                </tr>
+    `;
+}
+    
